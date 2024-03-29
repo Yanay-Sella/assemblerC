4dc6ab556aae7b8fd6c541b87f0c758129d2867c
Yanay Sella <86674014+Yanay-Sella@users.noreply.github.com>
2024-03-29 13:38:46 +0300

    directive detection
    need to store the values in a dataArray

--- global.h
+++ global.h
@@ -3,7 +3,7 @@
 #define MAX_SYMBOLS 4096
 
 typedef struct {
-    char symbolname[MAX_LINE_LENGTH];
+    char name[MAX_LINE_LENGTH];
     char identifier[MAX_LINE_LENGTH];
     int value;
 } Symbol;
--- sym_scan.c
+++ sym_scan.c
@@ -4,48 +4,66 @@
 void scanSymbols(FILE *file, Symbol *symbolTable) {
     char line[MAX_LINE_LENGTH];
     int address = 0;
-    int symbolCount = 0;
+    int dataCount = 0;
+    int instCound = 0;
 
     while (fgets(line, sizeof(line), file)) {
+        /* symbol properties */
         char symbolName[MAX_LINE_LENGTH];
-        int value;
+        char symbolIdentifier[10];
+        int symbolValue;
+        /* found symbol flag */
         int foundSymbol = 0;
 
         /* Parse the line according to a .define line */
         if (strncmp(line, ".define", 7) == 0) {
             foundSymbol = 1;
-            sscanf(line, ".define %s = %d", symbolName, &value);
+            strncpy(symbolIdentifier, "mdefine");
+            sscanf(line, ".define %s = %d", symbolName, &symbolValue);
         }
 
-        /* saving the pointer for the ":" in the line */
+        /* saving the pointer for the ":" in the line if exists */
         char *colonPosition = strchr(line, ':');
         /* if its a symbol defenition like "SYMBOL: ____" */
         if (colonPosition) {
             foundSymbol = 1;
+
             /* copying the symbol name from the line */
             strncpy(symbolName, line, colonPosition - line);
             symbolName[colonPosition - line] = '\0';
 
-            // TODO: Parse the rest of the line to determine the value and identifier
+            /* move the pointer to the start of the directive */
+            char *directiveStart = colonPosition + 1;
+
+            /* check for .data or .string directive */
+            if (strncmp(directiveStart, ".data ", 6) == 0 || strncmp(directiveStart, ".string ", 8) == 0) {
+                strcpy(symbolIdentifier, "data");
+                symbolValue = dataCount;
+            }
+
+            /* check for .extern or .entry directive */
+            else if (strncmp(directiveStart, ".extern ", 8) == 0 || strncmp(directiveStart, ".entry ", 7) == 0) {
+                // TODO: Handle .extern or .entry directive
+            }
         }
 
         if(foundSymbol == 1){
-            /* Check if the symbol already exists in the symbol table */
+            /* check for conflicts in the symbol table */
             int i;
-            for (i = 0; i < symbolCount; i++) {
-                if (strcmp(symbolTable[i].symbolname, symbolName) == 0) {
-                    /* Symbol already exists, handle as needed */
+            for (i = 0; i < dataCount; i++) {
+                if (strcmp(symbolTable[i].name, symbolName) == 0) {
                     printf("Error: Symbol %s already defined.\n", symbolName);
                     return;
                 }
             }
 
-            /* Add the symbol to the symbol table */ 
-            strcpy(symbolTable[symbolCount].symbolname, symbolName);
-            symbolTable[symbolCount].value = value;
-            strcpy(symbolTable[symbolCount].identifier, "mdefine");
+            /* add the symbol to the symbol table at dataCount */ 
+            strcpy(symbolTable[dataCount].name, symbolName);
+            symbolTable[dataCount].value = symbolValue;
+            strcpy(symbolTable[dataCount].identifier, symbolIdentifier);
 
-            symbolCount++;
+            /* increase DC for the next symbol */
+            dataCount++;
         }
     }
 }
